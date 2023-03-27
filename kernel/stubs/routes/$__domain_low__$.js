bedd8ef2cd5f33723d5c5e8cf83384d8a60de78a
Vitor Esposito <vitor_esposito@hotmail.com>
2023-03-27 18:13:46 -0300

    Correção no import do filtro

--- kernel/stubs/routes/$__domain_low__$.js
+++ kernel/stubs/routes/$__domain_low__$.js
@@ -2,7 +2,7 @@ import {
   fields,
   order
 } from '#src/domains/$__domain_low__$/parameters/$__domain__$ListParameter.js';
-import { filters } from '#src/domains/$__domain_low__$/filters/$__domain__$Filter.js';
+import { filter } from '#src/domains/$__domain_low__$/filters/$__domain__$Filter.js';
 import FilterMiddleware from '#src/middlewares/FilterMiddleware.js';
 import RequestParamsMiddleware from '#src/middlewares/RequestParamsMiddleware.js';
 import JwtMiddleware from '#src/middlewares/JwtMiddleware.js';
@@ -27,7 +27,7 @@ const $__domain_low__$Router = Router();
 
 $__domain_low__$Router.use(new JwtMiddleware().process);
 $__domain_low__$Router.use(new RequestParamsMiddleware().process(fields, order));
-$__domain_low__$Router.use(new FilterMiddleware().process(filters));
+$__domain_low__$Router.use(new FilterMiddleware().process(filter));
 
 $__domain_low__$Router.get(
   '/list',
